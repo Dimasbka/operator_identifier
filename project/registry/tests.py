52cae17f84e5977aff898030639a2d7ff0d55ab0
Dmitry Moroz <dimasbka@gmail.com>
2024-04-23 22:13:27 +0300

    переделал блокировки pid файла причесал код

--- project/registry/tests.py
+++ project/registry/tests.py
@@ -1,5 +1,5 @@
-
 from .models import OperatorProvider
+
 # Create your tests here.
 
 from unittest import TestCase
@@ -10,26 +10,20 @@ class OperatorProvider_TestCase(TestCase):
         self.op = OperatorProvider()
 
     def test_OperatorProvider_valid_number1(self):
-        self.assertTrue( self.op.is_valid_number('79173453223'))
+        self.assertTrue(self.op.is_valid_number("79173453223"))
 
     def test_OperatorProvider_valid_number2(self):
-        self.assertTrue( self.op.is_valid_number('79067735629'))
+        self.assertTrue(self.op.is_valid_number("79067735629"))
 
     def test_OperatorProvider_in_valid_number1(self):
-        self.assertFalse( self.op.is_valid_number('89173453223'))
+        self.assertFalse(self.op.is_valid_number("89173453223"))
 
     def test_OperatorProvider_invalid_number2(self):
-        self.assertFalse( self.op.is_valid_number('891453223'))
+        self.assertFalse(self.op.is_valid_number("891453223"))
 
     def test_OperatorProvider_invalid_number3(self):
-        self.assertFalse( self.op.is_valid_number('7917345373453734223'))
+        self.assertFalse(self.op.is_valid_number("7917345373453734223"))
 
     def test_OperatorProvider_404_number(self):
-        self.assertFalse( self.op.get_data('70000000000'))
-        self.assertTrue( self.op.status == 404)
-
-
-
-
-
-
+        self.assertFalse(self.op.get_data("70000000000"))
+        self.assertTrue(self.op.status == 404)
--- project/registry/views.py
+++ project/registry/views.py
@@ -9,59 +9,53 @@ from django.views import View
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class PhoneNumberVerificationForm (forms.Form):
-    phone_numder = forms.CharField(
-                label='номер абонента',
-                error_messages={"incomplete": "Введите номер телефона"},
-                validators=[RegexValidator(r"^7[0-9]{10,}$", "Неправильный формат номер ")],
-                help_text='введите российский номер в формате MSISDN (например, 79173453223)',
-            )
+
+class PhoneNumberVerificationForm(forms.Form):
+    phone_number = forms.CharField(
+        label="номер абонента",
+        error_messages={"incomplete": "Введите номер телефона"},
+        validators=[RegexValidator(r"^7[0-9]{10,}$", "Неправильный формат номер ")],
+        help_text="введите российский номер в формате MSISDN (например, 79173453223)",
+    )
+
 
 class FindPhoneNumber(View):
-    http_method_names = ["post","get"]
+    http_method_names = ["post", "get"]
 
     def post(self, request, *args, **kwargs):
-        form = PhoneNumberVerificationForm( request.POST, )
+        form = PhoneNumberVerificationForm(
+            request.POST,
+        )
         if form.is_valid():
             form_data = form.cleaned_data
             op = OperatorProvider()
-            data = op.get_data(form_data['phone_numder'])
+            data = op.get_data(form_data["phone_number"])
 
             if not data:
-                data = {'error': op.error }
+                data = {"error": op.error}
 
-        return render(request,'index.html',{'form':form,'data':data} )
-
+        return render(request, "index.html", {"form": form, "data": data})
 
     def get(self, request, *args, **kwargs):
         form = PhoneNumberVerificationForm()
-        return render(request,'index.html',{'form':form } )
-
+        return render(request, "index.html", {"form": form})
 
 
 class FindPhoneNumberAPI(APIView):
 
     def get(self, request, format=None):
 
-        if ('phone_numder' not in request.GET) or (request.GET['phone_numder']==''):
-            return Response ({
-                'error':"Не передан обязательный параметр 'phone_numder'"
-            }, status=400)
+        if ("phone_number" not in request.GET) or (request.GET["phone_number"] == ""):
+            return Response(
+                {"error": "Не передан обязательный параметр 'phone_number'"}, status=400
+            )
 
-        phone_numder = request.GET['phone_numder']
+        phone_number = request.GET["phone_number"]
 
         op = OperatorProvider()
-        op_data = op.get_data(phone_numder)
+        op_data = op.get_data(phone_number)
 
         if op_data:
             return Response(op_data)
         else:
-            return Response({
-                'error':op.error
-            }, status=op.status)
-
-
-
-
-
-
+            return Response({"error": op.error}, status=op.status)
